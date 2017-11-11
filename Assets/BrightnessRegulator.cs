f1934cdd57ff38bae0e1fc3ba0b415b9847d2e01
Tsubasa.Oumi <ohmiwing@gmail.com>
2017-11-11 14:33:10 +0900

    オブジェクトにぶつかったら光る設定
    スクリプトがオブジェクトにアタッチできないので、対応策を考え中。

--- Assets/BrightnessRegulator.cs
+++ Assets/BrightnessRegulator.cs
@@ -9,13 +9,13 @@ public class BrightnessRegulator : MonoBehaviour {
     // Emissionの最小値
     private float minEmission = 0.3f;
     // Emissionの強度
-    private float magEmission = 2.0f;
+    private float magEmission = 0.2f;
     // 角度
     private int degree = 0;
     //発光速度
     private int speed = 10;
     // ターゲットのデフォルトの色
-    Color defaultColor = Color.white;
+    Color defaultColor = Color.white; //.は「の」って意味。ここでは「色の白」だね。
 
     // Use this for initialization
     void Start()
@@ -24,13 +24,13 @@ public class BrightnessRegulator : MonoBehaviour {
         // タグによって光らせる色を変える
         if (tag == "SmallStarTag")
         {
-            this.defaultColor = Color.white;
+            this.defaultColor = Color.white; //thisはこのクラス内にある変数を使いますよとわかりやすくするために使う（この場合、BrightnessRegulatorクラス）
         }
-        else if (tag == "LargeStarTag")
+        if (tag == "LargeStarTag")
         {
             this.defaultColor = Color.yellow;
         }
-        else if (tag == "SmallCloudTag" || tag == "LargeCloudTag")
+        if (tag == "SmallCloudTag" || tag == "LargeCloudTag")
         {
             this.defaultColor = Color.blue;
         }
@@ -39,8 +39,8 @@ public class BrightnessRegulator : MonoBehaviour {
         this.myMaterial = GetComponent<Renderer>().material;
 
         //オブジェクトの最初の色を設定
-        myMaterial.SetColor("_EmissionColor", this.defaultColor * minEmission);
-    }
+        myMaterial.SetColor("_EmissionColor", minEmission * this.defaultColor);
+}
 
     // Update is called once per frame
     void Update()
