20746cbb149f75df81d7a4aa35e90fdbd0f4a14a
Tsubasa.Oumi <ohmiwing@gmail.com>
2017-11-14 22:39:39 +0900

    課題：得点を表示しよう完了
    下記の条件を満たしてください
    
    Lesson5で作成したPinBallをもとに作成してください
    UIのTextを使って得点を表示してください
    ターゲット（大小の星と雲）にボールが衝突した時に得点を加算してください
    ターゲットの種類によって取得できる点数を変えてください（例：小さい星は10点、大きい星は20点など）
    得点は画面の右上に見やすく表示しましょう
    上記完了。

--- Assets/ScoreController.cs
+++ Assets/ScoreController.cs
@@ -11,47 +11,45 @@ public class ScoreController : MonoBehaviour
     //点数を表示するテキスト
     private GameObject gamescoreText;
 
-    // Use this for initialization
-    void Start()
-    {
-        //シーン中のScoreTextオブジェクトを取得
-        this.gamescoreText = GameObject.Find("ScoreText");
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-        //③　ターゲット（大小の星と雲）にボールが衝突した時に得点を加算してください☚どの関数を使うかとか
-        //これを表現するためにどうするべきかを考える。
-        //疑問：どうやって1.ターゲット（大小の星と雲）にボールが衝突した時を取得する？
-        //→void OnCollisionEnter(Collision other)を使って
-        // ヒント：OnCollisionEnterでボールがターゲットに衝突したことを検知できると思いますので、OnCollisionEnter関数内で
-
-        //1.何のターゲット（大小の星と雲）に衝突したかをif文を用いて分岐して検知する
-        //2.ターゲットごとの得点をスコア用の変数に加算する
-        //3.その変数の値をテキストに反映する
-
-        // タグによって光らせる色を変える　☚これとvoid OnCollisionEnter(Collision other)を使う。
-        // void OnCollisionEnter(Collision other)内でこれを分岐して検知する
 
 
-        //④　ターゲットの種類によって取得できる点数を変えてください（例：小さい星は10点、大きい星は20点など）
-        //⑤　得点は画面の右上に見やすく表示しましょう：完了
+    //③　ターゲット（大小の星と雲）にボールが衝突した時に得点を加算してください☚どの関数を使うかとか
+    //これを表現するためにどうするべきかを考える。
+    //疑問：どうやって1.ターゲット（大小の星と雲）にボールが衝突した時を取得する？
+    //→void OnCollisionEnter(Collision other)を使って
+    // ヒント：OnCollisionEnterでボールがターゲットに衝突したことを検知できると思いますので、OnCollisionEnter関数内で
 
-        //GameoverTextに点数を表示
-        //this.gameoverText.GetComponent<Text>().text = "Game Over";
-    }
+    //1.何のターゲット（大小の星と雲）に衝突したかをif文を用いて分岐して検知する
+    //2.ターゲットごとの得点をスコア用の変数に加算する
+    //3.その変数の値をテキストに反映する
+
+    // タグによって光らせる色を変える　☚これとvoid OnCollisionEnter(Collision other)を使う。
+    // void OnCollisionEnter(Collision other)内でこれを分岐して検知する
+
+
+    //④　ターゲットの種類によって取得できる点数を変えてください（例：小さい星は10点、大きい星は20点など）
+    //⑤　得点は画面の右上に見やすく表示しましょう：完了
+
+    //SmallStarTagがついているオブジェクトの点数
+    //int i = 0;
+    //int SmallStarTagObject = 0;
+    //int LargeStarTagObject = 0;
+    //int SmallCloudTagObject = 0;
+    //int LargeCloudTagObject = 0;
+    int totalpoints = 0;
+    //GameoverTextに点数を表示
+    //this.gameoverText.GetComponent<Text>().text = "Game Over";
     //衝突時に呼ばれる関数 
     //`other`はCollision型の引数なので `衝突した相手`のCollision情報が入っています。
     void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "SmallStarTag")  //☚この文でSmallStarTagに当たった時が表現できるはず
         {
-                if (tag == "SmallStarTag")  //☚この文でSmallStarTagに当たった時が表現できるはず
-            {
-                //わからないのは得点の表示の仕方。それは確かBallControllerのところでやったはず。確認
-                this.gamescoreText.GetComponent<Text>().text = "1";  //☚1が表示されるかまず試す。
-                //↑で数字使いたいが、stringしか使えない。どうする？
+                  //     やりたいこと： SmallStarTagObject = 0にOnCollisionEnterが呼ばれるごとに＋１すること                                                     //↑で数字使いたいが、stringしか使えない。どうする？
+            //SmallStarTagObject += 1; //あ、じゃあOnCollisionEnterの中にSmallStarTagObjectに1足すようにすればいいや。
+            totalpoints += 1;
+            //わからないのは得点の表示の仕方。それは確かBallControllerのところでやったはず。確認
+            this.gamescoreText.GetComponent<Text>().text = totalpoints.ToString();  //☚1が表示されるかまず試す。☚変数を使えば！？
 
                 //else if (tag == "LargeStarTag")
                 //{
@@ -61,10 +59,45 @@ public class ScoreController : MonoBehaviour
                 //{
                 //    this.defaultColor = Color.blue;
                 //}
-
-            //衝突した対象のタグをコンソールに表示する
-            //Debug.Log(other.gameObject.tag);
-            }
         }
-    //↑これだけではDebug.Logの()内の変数を表示しなかったので、そこから11/12は考える
+
+        if (other.gameObject.tag == "LargeStarTag")
+        {
+            //LargeStarTagObject += 10;
+            totalpoints += 10;
+            this.gamescoreText.GetComponent<Text>().text = totalpoints.ToString();
+        }
+        if (other.gameObject.tag == "SmallCloudTag")
+        {
+            //SmallCloudTagObject += 1;
+            totalpoints += 1;
+            this.gamescoreText.GetComponent<Text>().text = totalpoints.ToString();
+        }
+
+        if (other.gameObject.tag == "LargeCloudTag")
+        {
+            //LargeCloudTagObject += 10;
+            totalpoints += 10;
+            this.gamescoreText.GetComponent<Text>().text = totalpoints.ToString();
+        }
+        //別なオブジェクトに当たったら、点数が0に戻ってしまう。
+        //変数をtotalpointsに統一したら、加算されるようになった。（前のやつだとそれぞれの変数<LargeCloudTagObjectとか>に
+        //ばらばらに数字が足されるので、別なオブジェクトに当たるたびに0からtotalpointsがやり直される）
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        //シーン中のScoreTextオブジェクトを取得
+        this.gamescoreText = GameObject.Find("ScoreText");
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
